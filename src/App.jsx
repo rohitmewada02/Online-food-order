56a41802da09e79e991b2ec364148908b0d90148
rohit <rohitmewadayyy@gamil.com>
2025-12-10 11:08:50 +0530

    Day 2 update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Register from './components/register/Register'
+import Login from './components/login/Login'
+import Home from './components/home/Home'
+import Card from './components/card/Card'
+import Profile from './components/profile/Profile'
+import Alldish from './components/alldish/Alldish'
 
 function App() {
   return (
     <div>
-      <h1>Create new project</h1>
+         <Routes>
+                <Route path='/' element={<Register/>} />
+                <Route path='/' element={<Login/>} />
+                <Route path='/' element={<Home/>} />
+                <Route path='/' element={<Card/>} />
+                <Route path='/' element={<Profile/>} />
+                <Route path='/' element={<Alldish/>} />
+
+
+         </Routes>
     </div>
+      
   )
 }
 
---
+++ src/components/register/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>
+      <h1>These is Register page</h1>
+    </div>
+  )
+}
+
+export default Register
